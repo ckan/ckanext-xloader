82bb876e3e7b93a55878e00b3f901a51d2063e18
Jay Guo <jguo144@gmail.com>
2018-10-01 17:44:22 -0400

    [OD-767] Add datastore_search_sql cache purge script to xloader (#2)
    * Add script to clear nginx cache
    
    * Set default for datastore_cache_dir
    
    The default value for datastore_cache_dir is /tmp/datastore_cache/. If the datastore cache directory doesn't exist the purge script does not run.

--- ckanext/xloader/jobs.py
+++ ckanext/xloader/jobs.py
@@ -9,6 +9,9 @@ import datetime
 import traceback
 import sys
 
+import subprocess
+from os import path
+
 import requests
 from rq import get_current_job
 import sqlalchemy as sa
@@ -35,6 +38,8 @@ MAX_CONTENT_LENGTH = config.get('ckanext.xloader.max_content_length') or 1e9
 CHUNK_SIZE = 16 * 1024  # 16kb
 DOWNLOAD_TIMEOUT = 30
 
+DATASTORE_CACHE_DIR = config.get('datastore_cache_dir') or \
+                      '/tmp/datastore_cache/'
 
 # 'api_key': user['apikey'],
 # 'job_type': 'push_to_datastore',
@@ -280,6 +285,20 @@ def xloader_data_into_datastore_(input, job_dict):
         logger.info('Finished loading with messytables')
 
     tmp_file.close()
+
+    # Purge nginx cache based on resource id
+    if path.isdir(DATASTORE_CACHE_DIR):
+        # Path of purge script
+        base_path = path.abspath(path.dirname(__file__))
+        purge_script = path.join(base_path, 'nginx-cache-purge.sh')
+
+        # Run script as subprocess
+        purge_params = [purge_script, resource_id, DATASTORE_CACHE_DIR]
+        purge_process = subprocess.Popen(purge_params,
+                                         stdout=subprocess.PIPE,
+                                         stderr=subprocess.STDOUT)
+        purge_result = purge_process.stdout.read()
+        logger.info('Purge cache: {result}'.format(result=purge_result))
 
     logger.info('Express Load completed')
 
