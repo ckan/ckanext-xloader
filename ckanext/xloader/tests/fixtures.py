659ba48e85f8f5db8989cb8f6dfeec6badeff3f5
Brett <54408245+kowh-ai@users.noreply.github.com>
2021-05-03 09:37:19 +0200

    Update fixtures.py

--- ckanext/xloader/tests/fixtures.py
+++ ckanext/xloader/tests/fixtures.py
@@ -162,6 +162,11 @@ except ImportError:
         with test_request_context():
             yield
 
+    @pytest.fixture()
+    def full_reset(reset_db):
+        reset_db()
+        reset_datastore_db()
+        add_full_text_trigger_function()
 
 def reset_datastore_db():
     engine = get_write_engine()
@@ -182,8 +187,4 @@ def add_full_text_trigger_function():
     Session.remove()
 
 
-@pytest.fixture()
-def full_reset(reset_db):
-    reset_db()
-    reset_datastore_db()
-    add_full_text_trigger_function()
+
