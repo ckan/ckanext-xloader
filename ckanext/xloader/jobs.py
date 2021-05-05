51cb50c7439c451fce47e72102be2bb82683f10e
Brett <54408245+kowh-ai@users.noreply.github.com>
2021-05-05 09:30:54 +0200

    Update jobs.py

--- ckanext/xloader/jobs.py
+++ ckanext/xloader/jobs.py
@@ -481,8 +481,8 @@ def update_resource(resource, patch_only=False):
     """
     action = 'resource_update' if not patch_only else 'resource_patch'
     from ckan import model
-    site_user = get_action('get_site_user')({'model': model, 'ignore_auth': True}, {})
-    context = {'model': model, 'session': model.Session, 'ignore_auth': True, 'user': site_user['name'], 'auth_user_obj': None}           
+    user = get_action('get_site_user')({'model': model, 'ignore_auth': True}, {})
+    context = {'model': model, 'session': model.Session, 'ignore_auth': True, 'user': user['name'], 'auth_user_obj': None}           
     get_action(action)(context, resource)
 
 
