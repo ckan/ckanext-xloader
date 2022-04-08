987fd03e9031a617c6cca6cf88b4d28799c9e998
pdelboca <patriciodelboca@gmail.com>
2022-04-08 13:23:30 -0300

    Do not add full_text_trigger twice

--- ckanext/xloader/tests/fixtures.py
+++ ckanext/xloader/tests/fixtures.py
@@ -3,6 +3,7 @@ import sqlalchemy
 import sqlalchemy.orm as orm
 import os
 
+from ckan.plugins import toolkit
 from ckanext.datastore.tests import helpers as datastore_helpers
 from ckanext.xloader.loader import get_write_engine
 
@@ -161,28 +162,14 @@ except ImportError:
         with test_request_context():
             yield
 
+
 def reset_datastore_db():
     engine = get_write_engine()
     Session = orm.scoped_session(orm.sessionmaker(bind=engine))
     datastore_helpers.clear_db(Session)
 
 
-def add_full_text_trigger_function():
-    engine = get_write_engine()
-    Session = orm.scoped_session(orm.sessionmaker(bind=engine))
-    c = Session.connection()
-    with open(
-        os.path.join(__location__, "..", "..", "..", "full_text_function.sql"),
-        "r",
-    ) as full_text_sql:
-        c.execute(sqlalchemy.text(full_text_sql.read()))
-    Session.commit()
-    Session.remove()
-
 @pytest.fixture()
 def full_reset(reset_db):
     reset_db()
     reset_datastore_db()
-    add_full_text_trigger_function()
-
-
