5d1a1d40949e66e626469c8a9b5d809f366749e2
Rae Knowler <rae.knowler@liip.ch>
2022-10-26 16:41:48 +0200

    test: Test different behaviour in Py2 and Py3

--- ckanext/xloader/tests/test_loader.py
+++ ckanext/xloader/tests/test_loader.py
@@ -3,6 +3,7 @@ from __future__ import print_function
 from __future__ import absolute_import
 import os
 import pytest
+import six
 import sqlalchemy.orm as orm
 import datetime
 import logging
@@ -783,7 +784,12 @@ class TestLoadUnhandledTypes(TestLoadBase):
             in str(exception.value)
         )
 
-    def test_shapefile_zip(self):
+    @pytest.mark.skipif(
+        six.PY3,
+        reason="In Python 3, tabulator will unzip archives and load the first "
+               "file found (if possible)."
+    )
+    def test_shapefile_zip_python2(self):
         filepath = get_sample_filepath("polling_locations.shapefile.zip")
         resource_id = "test1"
         factories.Resource(id=resource_id)
@@ -794,6 +800,32 @@ class TestLoadUnhandledTypes(TestLoadBase):
                 mimetype="text/csv",
                 logger=logger,
             )
+
+    @pytest.mark.skipif(
+        six.PY2,
+        reason="In Python 2, tabulator will not load a zipped archive, so the "
+               "loader will raise a LoaderError."
+    )
+    def test_shapefile_zip_python3(self, Session):
+        # tabulator unzips the archive and tries to load the first file it
+        # finds, 'Polling_Locations.cpg'. This file only contains the
+        # following data: `UTF-8`.
+        filepath = get_sample_filepath("polling_locations.shapefile.zip")
+        resource_id = "test1"
+        factories.Resource(id=resource_id)
+        loader.load_csv(
+            filepath,
+            resource_id=resource_id,
+            mimetype="text/csv",
+            logger=logger,
+        )
+
+        assert self._get_records(Session, "test1") == []
+        assert self._get_column_names(Session, "test1") == [
+            '_id',
+            '_full_text',
+            'UTF-8'
+        ]
 
 
 class TestLoadTabulator(TestLoadBase):
