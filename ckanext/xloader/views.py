05b2b888a2e6772e019a84f59ee33145dc93e99e
antuarc <carl.antuar@smartservice.qld.gov.au>
2023-05-19 12:19:05 +1000

    [QOLSVC-1863] make XLoader log truncation configurable
    - Start with the first and last 50 rows, provide a link to double it

--- ckanext/xloader/views.py
+++ ckanext/xloader/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, request
 
 import ckanext.xloader.utils as utils
 
@@ -12,4 +12,10 @@ def get_blueprints():
 
 @xloader.route("/dataset/<id>/resource_data/<resource_id>", methods=("GET", "POST"))
 def resource_data(id, resource_id):
-    return utils.resource_data(id, resource_id)
+    rows = request.args.get('rows')
+    if rows:
+        try:
+            rows = int(rows)
+        except ValueError:
+            rows = None
+    return utils.resource_data(id, resource_id, rows)
