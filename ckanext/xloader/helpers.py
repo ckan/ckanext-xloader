8ff2e9fb00a36bf2c49620dcca090a9a6f2abc81
JVickery-TBS <jesse.vickery@tbs-sct.gc.ca>
2023-06-15 17:24:33 +0000

    feat(templates): added conditional DataStore action to more templates, added new helper;
    - Added the conditional DataStore action link to more templates.
    - Added another new helper to handle possible non-existant core helper.

--- ckanext/xloader/helpers.py
+++ ckanext/xloader/helpers.py
@@ -31,3 +31,10 @@ def is_xloader_format(resource_format):
     from ckanext.xloader.plugin import XLoaderFormats
 
     return XLoaderFormats.is_it_an_xloader_format(resource_format)
+
+
+def is_xloader_type(resource_url_type):
+    try:
+        return resource_url_type not in toolkit.h.datastore_rw_resource_url_types()
+    except AttributeError:
+        return (resource_url_type == 'upload' or resource_url_type == '')
--- ckanext/xloader/plugin.py
+++ ckanext/xloader/plugin.py
@@ -212,4 +212,5 @@ class xloaderPlugin(plugins.SingletonPlugin):
             "xloader_status": xloader_helpers.xloader_status,
             "xloader_status_description": xloader_helpers.xloader_status_description,
             "is_xloader_format": xloader_helpers.is_xloader_format,
+            "is_xloader_type": xloader_helpers.is_xloader_type,
         }
