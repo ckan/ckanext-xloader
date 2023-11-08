4baf587e9632436d06b92a4404104bedf6067dc4
Jesse Vickery <jesse.vickery@tbs-sct.gc.ca>
2023-11-08 19:51:31 +0000

    feat(views): added view and button to delete ds table;
    - Added view to delete datastore table.
    - Added button to delete a resource's datastore table.

--- ckanext/xloader/views.py
+++ ckanext/xloader/views.py
@@ -1,5 +1,8 @@
 from flask import Blueprint
 
+from ckanapi import LocalCKAN
+from ckan.plugins.toolkit import _, h, g, render, request, abort, NotAuthorized
+
 import ckanext.xloader.utils as utils
 
 
@@ -13,3 +16,37 @@ def get_blueprints():
 @xloader.route("/dataset/<id>/resource_data/<resource_id>", methods=("GET", "POST"))
 def resource_data(id, resource_id):
     return utils.resource_data(id, resource_id)
+
+
+@xloader.route("/dataset/<id>/delete-datastore-table/<resource_id>", methods=("GET", "POST"))
+def delete_datastore_table(id, resource_id):
+    if u'cancel' in request.form:
+        return h.redirect_to(u'xloader.resource_data', id=id, resource_id=resource_id)
+
+    if request.method == 'POST':
+        lc = LocalCKAN(username=g.user)
+
+        try:
+            lc.action.datastore_delete(
+                resource_id=resource_id,
+                force=True,
+            )
+        except NotAuthorized:
+            return abort(403, _(u'Unauthorized to delete resource %s') % resource_id)
+
+        h.flash_notice(_(u'DataStore table and Data Dictionary deleted for resource %s') % resource_id)
+
+        return h.redirect_to(
+            'xloader.resource_data',
+            id=id,
+            resource_id=resource_id
+        )
+    else:
+        g.resource_id = resource_id
+        g.package_id = id
+
+        extra_vars = {
+            u"resource_id": resource_id,
+            u"package_id": id
+        }
+        return render(u'xloader/confirm_datastore_delete.html', extra_vars)
