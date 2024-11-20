b5980e4a1c726c2e6d98f968424d0be7b1fc3cb0
amercader <amercadero@gmail.com>
2024-11-20 12:35:40 +0100

    Update README, migrate it to Markdown
    Reformatted the README as Markdown to make it easier to update. Did a
    full review and updated broken links and added config options to
    increase visibility (generated with `ckan config docs --enabled -f md`)

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the relevant file
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
@@ -19,7 +19,7 @@ setup(
 
     description='Express Loader - quickly load data into CKAN DataStore''',
     long_description=long_description,
-    long_description_content_type='text/x-rst',
+    long_description_content_type='text/markdown',
 
     # The project's main homepage.
     url='https://github.com/ckan/ckanext-xloader',
