8391f0150a2796e2ec9a3429429c94373fd9e3a7
ThrawnCA <carl.antuar@smartservice.qld.gov.au>
2026-03-09 16:49:03 +1000

    load config at plugin configure time instead of module load time, #270

--- ckanext/xloader/auth.py
+++ ckanext/xloader/auth.py
@@ -1,6 +1,6 @@
 from ckan import authz
 
-from .jobs import DEFAULT_QUEUE_NAMES
+from .jobs import default_queue_names
 
 import ckanext.datastore.logic.auth as auth
 
@@ -8,7 +8,7 @@ import ckanext.datastore.logic.auth as auth
 def xloader_submit(context, data_dict):
     # only sysadmins can specify a custom processing queue
     custom_queue = data_dict.get('queue')
-    if custom_queue and custom_queue not in DEFAULT_QUEUE_NAMES:
+    if custom_queue and custom_queue not in default_queue_names:
         return authz.is_authorized('config_option_update', context, data_dict)
     return auth.datastore_auth(context, data_dict)
 
